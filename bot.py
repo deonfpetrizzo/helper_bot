78212cd79de78c53ffbd3928a471de4dbffddded
Deon Petrizzo <deonpetrizzo@gmail.com>
2022-08-14 13:24:19 -0700

    refactored pull and push methods

--- bot.py
+++ bot.py
@@ -2,56 +2,59 @@ import os
 import sys
 import time 
 import webbrowser 
-from pomodoro import Pomodoro
+import traceback
+from timer import Timer
 from notemanager import NoteManager
 from constants import * 
 
 class PersonalBot:
     def __init__(self):
-        self.pom = Pomodoro()
-        self.n_mgr = NoteManager('resources/gkeep-login.json')
+        self.timer = Timer()
+        self.note_manager = NoteManager('resources/gkeep-login.json')
 
     def run_cmd(self, cmd):
         '''parses string commands and arguments and runs the corresponding functions'''
-        cmds = {
-            'mail' : self.mail,
-            'm' : self.mail,
-            'calculator' : self.calculator,
-            'c' : self.calculator,
-            'school' : self.school,
-            's' : self.school,
-            'github' : self.github,
-            'gh' : self.github,
-            'monkeytype' : self.monkeytype,
-            'mt' : self.monkeytype,
-            'spotify' : self.spotify,
-            'sp' : self.spotify,
-            'notepad' : self.notepad,
-            'np' : self.notepad,
-            'notes.pull' : self.n_mgr.pull, 
-            'n.pull' : self.n_mgr.pull,
-            'notes.push' : self.n_mgr.push,
-            'n.push' : self.n_mgr.push,
-            'pomodoro' : self.pom.start_timer_thread,
-            'p' : self.pom.start_timer_thread,
-            'help' : self.help,
-            'h' : self.help,
-            'quit' : sys.exit,
-            'q' : sys.exit,
-            'exit' : sys.exit,
-            'e' : sys.exit
-        }
-    
+        cmds = {'mail' : self.mail,
+                'm' : self.mail,
+                'calculator' : self.calculator,
+                'c' : self.calculator,
+                'school' : self.school,
+                's' : self.school,
+                'github' : self.github,
+                'gh' : self.github,
+                'monkeytype' : self.monkeytype,
+                'mt' : self.monkeytype,
+                'spotify' : self.spotify,
+                'sp' : self.spotify,
+                'notepad' : self.notepad,
+                'np' : self.notepad,
+                'notes.pull' : self.note_manager.pull, 
+                'n.pull' : self.note_manager.pull,
+                'notes.push' : self.note_manager.push,
+                'n.push' : self.note_manager.push,
+                'timer' : self.timer.regular,
+                't' : self.timer.regular,
+                'pomodoro' : self.timer.pomodoro,
+                'p' : self.timer.pomodoro,
+                'help' : self.help,
+                'h' : self.help,
+                'quit' : sys.exit,
+                'q' : sys.exit,
+                'exit' : sys.exit,
+                'e' : sys.exit}
         try:
             tkns = cmd.split()
-            if len(tkns) > 1:
-                cmds[tkns[0].lower()](float(tkns[1]), float(tkns[2]))
-            else:
+            if len(tkns) == 1:
                 cmds[tkns[0].lower()]()
-        except KeyError:
-            print('invalid cmd')
+            elif len(tkns) == 2:
+                cmds[tkns[0].lower()](float(tkns[1]))
+            else:
+                cmds[tkns[0].lower()](float(tkns[1]), float(tkns[2]))
         except TypeError:
-            print('enter args')
+            exception_data = traceback.format_exc().splitlines()
+            print(exception_data[len(exception_data)-1])
+        except KeyError:
+            print('invalid command')
         except IndexError:
             pass
             
@@ -85,7 +88,7 @@ class PersonalBot:
 
     def help(self):
         '''displays all available commands'''
-        dat = [['.command.', '.shortcut.', '.function.', '.arguments.'],
+        dat = [['.command.', '.shortcut.', '.function.'],
                ['mail', 'm', self.mail.__doc__],
                ['calculator', 'c', self.calculator.__doc__],
                ['school', 's', self.school.__doc__],
@@ -93,19 +96,16 @@ class PersonalBot:
                ['monkeytype', 'mt', self.monkeytype.__doc__],
                ['spotify', 'sp', self.spotify.__doc__],
                ['notepad', 'np', self.notepad.__doc__],
-               ['notes.push', 'n.push', self.n_mgr.push.__doc__],
-               ['notes.pull', 'n.pull', self.n_mgr.pull.__doc__],
-               ['pomodoro', 'p', self.pom.start_timer_thread.__doc__, 'work_minutes break_minutes'],
+               ['notes.push', 'n.push', self.note_manager.push.__doc__],
+               ['notes.pull', 'n.pull', self.note_manager.pull.__doc__],
+               ['timer', 't', self.timer.regular.__doc__],
+               ['pomodoro', 'p', self.timer.pomodoro.__doc__],
                ['quit', 'q', 'exit'],
                ['exit', 'e', 'exit']]
-
         for d in dat:
             row = ''
-            cnt = 0
             for e in d:
-                str_len = MAX_CMD_LEN+4 if cnt < 2 else MAX_COMMENT_LEN+4
-                row += e.ljust(str_len)
-                cnt += 1
+                row += e.ljust(MAX_CMD_LEN+4)
             print(row)
 
     def open_urls(self, urls):
--- notemanager.py
+++ notemanager.py
@@ -4,63 +4,55 @@ import gkeepapi
 
 class NoteManager:
     def __init__(self, acct_file_path):
-        with open(acct_file_path, 'r', encoding='utf-8') as f:
-            acct = json.load(f)
+        self.acct_file_path = acct_file_path
 
+    def login(self):
+        '''logs into gkeepapi using user credentials'''
+        with open(self.acct_file_path, 'r', encoding='utf-8') as f:
+            acct = json.load(f)
         self.keep = gkeepapi.Keep()
         self.keep.login(acct['username'], acct['password'])
 
     def get_txt_file_paths(self):
         '''returns a list of all txt file paths in the notes directory'''
         directory = 'notes'
-        txt_file_paths = []
-
-        for file_name in os.listdir(directory):
-            path = os.path.join(directory, file_name)
-
+        paths = []
+        for filename in os.listdir(directory):
+            path = os.path.join(directory, filename)
             if os.path.isfile(path):
-                txt_file_paths.append(str(path))
-        
-        return txt_file_paths
+                paths.append(path)
+        return paths
 
     def push(self):
-        '''transfers local txt file data to google keep'''
-        txt_file_paths = self.get_txt_file_paths()
+        '''transfers txt file data to gkeep'''
+        self.login()
+        paths = self.get_txt_file_paths()
         notes = self.keep.all() 
         does_note_exist = False
-
-        for txt_file_path in txt_file_paths:
-            title = str(txt_file_path)[txt_file_path.index('\\')+1 : txt_file_path.index('.')]
+        for path in paths:
+            title = path[path.index('\\')+1 : path.index('.')]
             body = ''
-
-            with open (txt_file_path, 'r') as f:
+            with open (path, 'r') as f:
                 body = f.read()
-
             for note in notes:
                 if note.title == title:
                     note.text = body
                     does_note_exist = True
                     break
                 does_note_exist = False
-
             if not does_note_exist:
                 self.keep.createNote(title, body)
-
-            self.keep.sync()
+        self.keep.sync()
 
     def pull(self):
-        '''updates the bodies of local txt files to match 
-                            those of corresponding google keep notes'''
-        txt_file_paths = self.get_txt_file_paths()
-        gnotes = self.keep.all()
-
-        for txt_file_path in txt_file_paths:
-            title = str(txt_file_path)[txt_file_path.index('\\')+1 : txt_file_path.index('.')]
-
+        '''updates txt files to match corresponding gkeep notes'''
+        self.login()
+        paths = self.get_txt_file_paths()
+        notes = self.keep.all()
+        for path in paths:
+            title = path[path.index('\\')+1 : path.index('.')]
             for note in notes:
                 if note.title == title:
-                    with open (txt_file_path, 'w') as f:
+                    with open (path, 'w') as f:
                         f.write(note.text)
                     break
-
-
