42c7d1f6aa0f318567175f55b7f7075f87e6b3c0
deonfpetrizzo <deonpetrizzo@gmail.com>
2023-08-01 10:31:39 -0700

    added -h option for specific usg statements

--- bot.py
+++ bot.py
@@ -63,37 +63,62 @@ class Bot:
         for option in options:
             if option == "-v":
                 self.portfolio = Portfolio("res/robinhood-login.json")
-                self.portfolio.value()
+                self.portfolio.performance()
             elif option == "-o":
                 open_urls(ROBINHOOD_URL)
             elif option == "-s":
                 pass
 
-    def __help(self):
+    def __help(self, base_cmd="help"):
         """displays all usages and their summaries"""
-        help_msg = (
-            f"{hl('checkin', 94)} [-m] [-s] [-S]\n"
+        checkin_usg = (
+            f"{hl('c, checkin', 94)} [{hl('-h', 93)}] | [-m -s -S]\n"
             "\t-m: open email accounts\n"
             "\t-s: open school related tabs\n"
-            "\t-S: open socials\n\n"
-            f"{hl('note', 94)} [-n] [-k] [-o dir_path | -i dir_path]\n"
+            "\t-S: open socials"
+        )
+        note_usg = (
+            f"{hl('n, note', 94)} [{hl('-h', 93)}] | [-n -k] [-o dir_path | -i dir_path]\n"
             "\t-n:          open notepad\n"
             "\t-k:          open gkeep\n"
             "\t-o dir_path: push notes from dir_path to gkeep\n"
             "\t-i dir_path: pull notes from gkeep to dir_path\n"
-            "\tdir_path:    path to txt file directory to push/pull to/from\n\n"
-            f"{hl('spotify', 94)} [-p [song] | -s [search_term]]\n"
+            "\tdir_path:    path to txt file directory to push/pull to/from"
+        )
+        spotify_usg = (
+            f"{hl('s, spotify', 94)} [{hl('-h', 93)}] | [-p [song] | -s [search_term]]\n"
             "\t-p:          play on open\n"
             "\t-s:          search on open\n"
             "\tsong:        name of song to play\n"
-            "\tsearch_term: name of artist/song/album/playlist to search\n\n"
-            f"{hl('stocks', 94)} [-v] [-o] [-s ticker]\n"
+            "\tsearch_term: name of artist/song/album/playlist to search"
+        )
+        stocks_usg = (
+            f"{hl('$, stocks', 94)} [{hl('-h', 93)}] | [-v -o -s ticker]\n"
             "\t-v:        show portfolio value and daily profit/loss\n"
             "\t-o:        open robinhood\n"
             "\t-s ticker: get information about a particular stock\n"
             "\tticker:    ticker/stock symbol"
         )
-        print(help_msg)
+        help_msg = (
+            f"{checkin_usg}\n\n"
+            f"{note_usg}\n\n"
+            f"{spotify_usg}\n\n"
+            f"{stocks_usg}\n\n"
+            f"{hl('-h', 93)}: print specific usage statement"
+        )
+        output_msg = {
+            "checkin": checkin_usg,
+            "c": checkin_usg,
+            "spotify": spotify_usg,
+            "s": spotify_usg,
+            "note": note_usg,
+            "n": note_usg,
+            "stocks": stocks_usg,
+            "$": stocks_usg,
+            "help": help_msg,
+            "h": help_msg
+        }
+        print(output_msg[base_cmd], end="\n\n")
 
     def __run(self):
         """parses string commands and assigns them to functions"""
@@ -104,11 +129,20 @@ class Bot:
         nopts = len(options)
         cmds = {
             "checkin": self.__checkin,
+            "c": self.__checkin,
             "spotify": self.__spotify,
+            "s": self.__spotify,
             "note": self.__note,
+            "n": self.__note,
             "stocks": self.__stocks,
-            "help": self.__help
+            "$": self.__stocks,
+            "help": self.__help,
+            "h": self.__help
         }  
+        for opt in options:
+            if opt == "-h":
+                self.__help(cmd)
+                return
         if nopts != 0 and nargs != 0:
             cmds[cmd](options, *args[:nargs])
         elif nopts != 0 and nargs == 0:
